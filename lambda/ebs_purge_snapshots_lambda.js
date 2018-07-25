4e1900996443bdbc584b5b3c76bb964f5c4c6405
Raja <bethleharaja@indix.com>
2018-07-25 18:20:10 +0530

    ebs-snapshot prge script updated

---
+++ lambda/ebs_purge_snapshots_lambda.js
@@ -0,0 +1,21 @@
+var ebs = require('./ebs');
+
+
+var handler = (event, context, callback) =>
+    
+  ebs.purgeSnapshotsInBatches(process.env.BATCH_SIZE || 100)
+  .then(()=> callback(null, 'Finished'))
+  .catch(callback);
+
+
+exports.handler = handler
+
+//Uncomment below to test locally
+exports.handler(null, null, function(e, s) {
+  if(e) {
+    console.log("[ERROR] ", e);
+    return;
+  }
+
+  console.log(s);
+});
