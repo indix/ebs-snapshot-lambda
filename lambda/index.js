78344b65e17ec3b95fa735b3442085d772dca360
Raja <bethleharaja@indix.com>
2018-07-27 14:01:19 +0530

    lambda module refactored Changes summary; - Unnecessary files removed - index.handler is set as lamda-handler

--- lambda/index.js
+++ lambda/index.js
@@ -1,20 +1,20 @@
-
 var ebs = require('./ebs');
 
 
 var handler = (event, context, callback) =>
     
-  ebs.snapshotVolumes()
+  ebs.purgeSnapshotsInBatches(process.env.BATCH_SIZE || 10)
+  .then(ebs.snapshotVolumes, ebs.snapshotVolumes)
   .then(()=> callback(null, 'Finished'))
   .catch(callback);
 
 
-exports.handler = handler;
+exports.handler = handler
 
-// Uncomment below to test locally
+// // Uncomment below to test locally
 // exports.handler(null, null, function(e, s) {
 //   if(e) {
-//     console.log("[ERROR] " + e);
+//     console.log("[ERROR] ", e);
 //     return;
 //   }
 
